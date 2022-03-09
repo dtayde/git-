c41ebf8be785344cae7bbfbc564adfdf36dd76c4
Dhanraj <dtayde4@gmail.com>
2022-03-09 09:38:47 +0530

    sql employee table created

---
+++ dhanraj.sql
@@ -0,0 +1,159 @@
+create table stu(id int,name varchar2(80),address varchar2(80));
+insert into stu(id,name,address)
+         VALUES(101,'dhanraj','pune');
+insert into stu(id,name,address)
+         VALUES(102,'yash','pune');  
+insert into stu(id,name,address)
+         VALUES(103,'shubham','nashik');
+insert into stu(id,name,address)
+        VALUES(104,'rohit','pue');
+        select * from stu;
+ insert into stu(id,name,address)
+        VALUES(105,'nikhil','mumbai');
+ insert into stu(id,name,address)
+        VALUES(106,'omkar','nagar');
+insert into stu(id,name,address)
+        VALUES(107,'yash','ram');
+        select * from stu;
+        DESCRIB stu;
+        desc stu;
+ create table emp2(no INT,name  VARCHAR2(80),job VARCHAR2(80),sal int,deptno INT) ;   
+ insert into emp2(no,name,job,sal,deptno)
+            values(1,'rohit','backend eng',80000,4);
+ insert into emp2(no,name,job,sal,deptno )
+         values(2,'aniket', 'software eng',60000,1);  
+ insert into emp2(no,name,job,sal,deptno )
+         values(3,'omkar', 'software eng',70000,3);  
+insert into emp2(no,name,job,sal,deptno )
+         values(4,'raju', 'front end eng',100000,1);      
+insert into emp2(no,name,job,sal,deptno )
+         values(5,'neha', 'backend eng',30000,1);
+insert into emp2(no,name,job,sal,deptno )
+         values(6,'sonal', 'testing',50000,1);  
+insert into emp2(no,name,job,sal,deptno )
+         values(7,'pooja', 'developer',50000,1);
+insert into emp2(no,name,job,sal,deptno )
+         values(8,'shambhavi', 'developer',50000,6);
+insert into emp2(no,name,job,sal,deptno )
+         values(9,'prajwal', 'software eng',80000,5); 
+insert into emp2(no,name,job,sal,deptno )
+         values(10,'hrishi', 'frontend eng',70000,7);
+insert into emp2(no,name,job,sal,deptno )
+         values(11,'rohit', 'software eng',75000,8);  
+         select * from emp1;
+insert into emp2(no,name,job,sal,deptno )
+         values(12,'pratik', 'software eng',90000,1);
+insert into emp2(no,name,job,sal,deptno )
+         values(13,'mahesh', 'software eng',60000,1);
+insert into emp2(no,name,job,sal,deptno )
+         values(14,'omkar', 'software eng',40000,3);    
+insert into emp2(no,name,job,sal,deptno )
+         values(15,'nilesh', 'backend eng',50000,2);
+insert into emp2(no,name,job,sal,deptno )
+         values(16,'rahul', 'software eng',90000,9);
+insert into emp2(no,name,job,sal,deptno )
+         values(17,'satish', 'software eng',30000,1);
+insert into emp2(no,name,job,sal,deptno )
+         values(18,'omkar', 'java developer ',50000,5);
+insert into emp2(no,name,job,sal,deptno )
+         values(19,'bharat', 'software eng',150000,1);
+insert into emp2(no,name,job,sal,deptno )
+         values(20,'ketan', 'software developer',70000,10);
+insert into emp2(no,name,job,sal,deptno )
+         values(21,'sidhu', 'backend eng',200000,12);  
+         select * from emp1;
+insert into emp2(no,name,job,sal,deptno)
+         values(22,'abhishek','associate eng',70000,11);
+       select * from emp1;
+ insert into emp2(no,name,job,sal,deptno)
+             values(23,'rahul','full stack eng',60000,11);
+ insert into emp2(no,name,job,sal,deptno)
+            values(23,'sameer','backend',40000,12);
+ insert into emp2(no,name,job,sal,deptno)
+           values(24,'amit','front end eng',35000,13);
+ insert into emp2(no,name,job,sal,deptno)
+                values(25,'roshan','testing',45000,9);
+ insert into emp2(no,name,job,sal,deptno)
+            values(25,'mahesh','tecnical asssistant',70000,12);
+ insert into emp2(no,name,job,sal,deptno)
+             values(26,'swapnil','backend eng',120000,13); 
+ insert into emp2(no,name,job,sal,deptno)
+             values(27,'pranit','inten',15000,14);
+ insert into emp2(no,name,job,sal,deptno)
+             values(28,'kapil','software eng',50000,12);
+  insert into emp2(no,name,job,sal,deptno)
+             values(29,'shilesh','backend eng',40000,14);
+ insert into emp2(no,name,job,sal,deptno)
+             values(30,'rohan','IT consultant',50000,14);
+  insert into emp2(no,name,job,sal, deptno)
+             values(31,'poonam','application developer',50000,13);  
+             select * from emp2;
+ insert into emp2(no,name,job,sal,deptno)
+             values(32,'varsha','web developer',90000,15);
+ insert into emp2(no,name,job,sal,deptno)            
+             values(33,'shashikant','web designer',60000,16);
+ insert into emp2(no,name,job,sal,deptno)
+            values(33,'ganesh','IT manager',150000,1);
+ insert into emp2(no,name,job,sal,deptno)
+            values(34,'suraj','multimedia programmer',70000,3);
+  insert into emp2(no,name,job,sal,deptno)      
+           values(35,'rajesh','game developer',80000,4);
+   insert into emp2(no,name,job,sal,deptno)
+           values(36,'gauri','system analyst',60000,6);
+ insert into emp2(no,name,job,sal,deptno)          
+         values(37,'aniket','software tester',70000,8);
+ insert into emp2(no,name,job,sal,deptno)
+          values(38,'neel','IT support',30000,16);
+ insert into emp2(no,name,job,sal,deptno)         
+        values(39,'roshan','web designer',70000,3);
+ insert into emp2(no,name,job,sal,deptno)
+        values(40 ,'hemant','software eng',40000,9);
+ insert into emp2(no,name,job,sal,deptno)
+        values( 41,'nikhil','web designer',50000,9) ;
+        select * from emp2;
+ insert into emp2(no,name,job,sal,deptno)
+        values(42 ,'sumit','front end eng',90000,17);
+insert into emp2(no,name,job,sal,deptno)
+        values(43 ,'rahul','backend eng',80000,16);       
+ insert into emp2(no,name,job,sal,deptno)
+        values( 44,'salman','full stack developer',100000,10);       
+ insert into emp2(no,name,job,sal,deptno)
+        values(45 ,'abhishek','webdeveloper',50000,14);       
+  insert into emp2(no,name,job,sal,deptno)
+        values(46 ,'tejas','testing',40000,15);      
+  insert into emp2(no,name,job,sal,deptno)
+        values( 47,'pavan','web designer',80000,18);      
+  insert into emp2(no,name,job,sal,deptno)
+        values(48 ,'varsha','front end',130000,20);      
+  insert into emp2(no,name,job,sal,deptno)
+        values( 49,'omkar','back end eng',80000,9);
+  insert into emp2(no,name,job,sal,deptno)
+        values(50 ,'rahul','multimedia manager',90000,18);  
+        select * from emp2;
+  insert into emp2(no,name,job,sal,deptno)
+        values(52 ,'somesh','tester',40000,19);      
+  insert into emp2(no,name,job,sal,deptno)
+        values(51 ,'ramakant','web developer',90000,20);
+  insert into emp2(no,name,job,sal,deptno)
+        values(53 ,'sunil','backend eng',60000,21);      
+   insert into emp2(no,name,job,sal,deptno)
+        values(54 ,'amir','software eng',70000,18);     
+        
+   insert into emp2(no,name,job,sal,deptno)
+        values(55 ,'madhuri','web designer',90000,20);     
+        
+  insert into emp2(no,name,job,sal,deptno)
+        values(56 ,'yogesh','full stack developer',70000,22);      
+  insert into emp2(no,name,job,sal,deptno)
+        values(57 ,'dimple','analyst ',60000,14);      
+  insert into emp2(no,name,job,sal,deptno)
+        values(58 ,'neha','tranee',12000,13);      
+  insert into emp2(no,name,job,sal,deptno)
+        values(59 ,'anjali','front end',90000,3);      
+  insert into emp2(no,name,job,sal,deptno)
+        values(60 ,'komal','data scientist',300000,4);      
+  insert into emp2(no,name,job,sal,deptno)
+        values( 61,'pranali','frontend developer',75000,12);  
+        select * from emp2;
+        delete from emp2 where no='23';
+        
